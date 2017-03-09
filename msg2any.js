8953213a69444e7de0bc3037b571eb71ecb431fd
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-03-09 10:13:37 +0900

    Support subfolder.

--- msg2any.js
+++ msg2any.js
@@ -1,31 +1,27 @@
 function puts(m){
-  WScript.echo(m);
+//  WScript.echo(m);
 }
-var shell = new ActiveXObject("WScript.shell");
-var BASE_DIR = shell.CurrentDirectory;
-
-function createFolder(fileName){
+function createFolder(dirPath){
   var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var dirName = fileName;
-  if(!fso.folderexists(dirName)){
-    puts(dirName);
-    fso.createFolder(dirName);
+  if(!fso.folderexists(dirPath)){
+    puts(dirPath);
+    fso.createFolder(dirPath);
   }
-  return(dirName);
+  return(dirPath);
 }
 
 var olSaveAsTypeMap ={
-  'olDoc'        : { value: 4  , ext: '.doc' }, // Microsoft Office Word 形式 (.doc)
+  'olDoc'        : { value: 4  , ext: '.doc'  } , // Microsoft Office Word 形式 (.doc)
   'olHTML'       : { value: 5  , ext: '.html' } , // HTML 形式 (.html)
-  'olICal'       : { value: 8  , ext: '.ics' } , // iCal 形式 (.ics)
-  'olMHTML'      : { value: 10 , ext: '.mht' } , // MIME HTML 形式 (.mht)
-  'olMSG'        : { value: 3  , ext: '.msg' } , // Outlook メッセージ形式 (.msg)
-  'olMSGUnicode' : { value: 9  , ext: '.msg' } , // Outlook Unicode メッセージ形式 (.msg)
-  'olRTF'        : { value: 1  , ext: '.rtf' } , // リッチ テキスト形式 (.rtf)
-  'olTemplate'   : { value: 2  , ext: '.oft' } , // Microsoft Outlook テンプレート (.oft)
-  'olTXT'        : { value: 0  , ext: '.txt' } , // テキスト形式 (.txt)
-  'olVCal'       : { value: 7  , ext: '.vcs' } , // VCal 形式 (.vcs)
-  'olVCard'      : { value: 6  , ext: '.vcf' }  // VCard 形式 (.vcf)
+  'olICal'       : { value: 8  , ext: '.ics'  } , // iCal 形式 (.ics)
+  'olMHTML'      : { value: 10 , ext: '.mht'  } , // MIME HTML 形式 (.mht)
+  'olMSG'        : { value: 3  , ext: '.msg'  } , // Outlook メッセージ形式 (.msg)
+  'olMSGUnicode' : { value: 9  , ext: '.msg'  } , // Outlook Unicode メッセージ形式 (.msg)
+  'olRTF'        : { value: 1  , ext: '.rtf'  } , // リッチ テキスト形式 (.rtf)
+  'olTemplate'   : { value: 2  , ext: '.oft'  } , // Microsoft Outlook テンプレート (.oft)
+  'olTXT'        : { value: 0  , ext: '.txt'  } , // テキスト形式 (.txt)
+  'olVCal'       : { value: 7  , ext: '.vcs'  } , // VCal 形式 (.vcs)
+  'olVCard'      : { value: 6  , ext: '.vcf'  }   // VCard 形式 (.vcf)
 };
 
 var MsgFile = function(msgFilePath){
@@ -38,7 +34,7 @@ var MsgFile = function(msgFilePath){
 
 MsgFile.prototype = {
   extract: function (){
-    var dirPath = createFolder(this.path.replace(".msg",""));
+    var dirPath = createFolder(this.path.replace(".msg","[MSG]"));
     var filePath = dirPath + "\\" + this.replaceInvalidChar(this._mailItem.subject) + this.saveType.ext;
     this._mailItem.SaveAs( filePath, this.saveType.value );
     var aEnum = new Enumerator(this._mailItem.attachments);
@@ -64,17 +60,27 @@ MsgFile.prototype = {
   }
 };
 
-function main(){
+function extractMsgFileInSubfolder(folderPath){
   var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var currentFolder = fso.getFolder(BASE_DIR);
-  var fe = new Enumerator(currentFolder.files);
-  for(; !fe.atEnd(); fe.moveNext()){
-    var fileName = fe.item().name;
+  var targetFolder = fso.getFolder(folderPath);
+  var fileEnum = new Enumerator(targetFolder.files);
+  for(; !fileEnum.atEnd(); fileEnum.moveNext()){
+    var fileName = fileEnum.item().name;
     if(fileName.match(/\.msg$/)){
-      var msgFile = new MsgFile(fe.item().path);
+      var msgFile = new MsgFile(fileEnum.item().path);
       msgFile.extract();
     }
   }
+  var folderEnum = new Enumerator(targetFolder.SubFolders);
+  for(; !folderEnum.atEnd(); folderEnum.moveNext()){
+    extractMsgFileInSubfolder(folderEnum.item().path);
+  }
+}
+
+function main(){
+  var shell = new ActiveXObject("WScript.shell");
+  var baseDir = shell.CurrentDirectory;
+  extractMsgFileInSubfolder(baseDir);
 }
 
 main();
