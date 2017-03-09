cea1af996f26c1461e8a67a2b2a2e58de7097102
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-03-09 13:17:53 +0900

    Support execution from double click.

--- msg2any.js
+++ msg2any.js
@@ -1,3 +1,16 @@
+(function(ws) {
+  if (ws.fullName.slice(-12).toLowerCase() !== '\\cscript.exe') {
+    var cmd = 'cscript.exe //nologo "' + ws.scriptFullName + '"';
+    var args = ws.arguments;
+    for (var i = 0, len = args.length; i < len; i++) {
+      var arg = args(i);
+      cmd += ' ' + (~arg.indexOf(' ') ? '"' + arg + '"' : arg);
+    }
+    new ActiveXObject('WScript.Shell').run(cmd);
+    ws.quit();
+  }
+})(WScript);
+
 function puts(m){
   WScript.echo(m);
 }
